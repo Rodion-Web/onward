64c3543e455d1c397fe058af4717910eea5a000c
Rodion-Web <rodeus770@gmail.com>
2024-12-02 17:03:22 +0300

    upd

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.getElementById("burger-menu").addEventListener("click", function () {
   document.querySelector(".nav-links").classList.toggle("active");
 });
+// Слайд
+const slider = document.querySelector(".testimonial-slider");
+
+// Прокрутка влево
+document.querySelector("#prev-btn").addEventListener("click", () => {
+  slider.scrollLeft -= slider.offsetWidth;
+});
+
+// Прокрутка вправо
+document.querySelector("#next-btn").addEventListener("click", () => {
+  slider.scrollLeft += slider.offsetWidth;
+});
